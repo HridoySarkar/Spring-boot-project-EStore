ec3f5e4581f5fecf2ce253d2299b4a2889ba2859
Hridoy <khanh4572@gmail.com>
2024-04-19 22:32:55 +0600

    Update

--- src/main/java/com/hridoy/estore/controllers/ProductsController.java
+++ src/main/java/com/hridoy/estore/controllers/ProductsController.java
@@ -3,6 +3,7 @@ package com.hridoy.estore.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +15,14 @@ import com.hridoy.estore.services.ProductsRepository;
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
-	
-	@Autowired
-	private ProductsRepository repo;
-	
-	@GetMapping({"","/"})
-	public String showProductList(Model model) {
-		List<product> products = repo.findAll();
-		model.addAttribute("products",products);
-		return "/product/index";
-	}
+    
+    @Autowired
+    private ProductsRepository repo;
+    
+    @GetMapping({"","/"})
+    public String showProductList(Model model) {
+        List<product> products = repo.findAll(Sort.by(Sort.Direction.DESC,"id"));
+        model.addAttribute("products",products);
+        return "/product/index";
+    }
 }
