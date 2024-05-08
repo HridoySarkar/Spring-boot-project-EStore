54ede38226cc62859b2dcca0628bde4c13a31efd
Hridoy Sarkar <75044534+HridoySarkar@users.noreply.github.com>
2024-05-08 23:44:52 +0600

    Delete src directory

--- src/main/java/com/hridoy/estore/services/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hridoy.estore.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hridoy.estore.models.product;
-
-public interface ProductsRepository extends JpaRepository<product, Integer> {
-	
-}
